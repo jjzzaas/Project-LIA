f02809ae1de571286d92f9894f637fc42892c8ed
jjzzaas <jojicheon1@gmail.com>
2026-07-15 23:47:47 +0900

    feat: enable chapter 4 routing

--- chapters/loader.js
+++ chapters/loader.js
@@ -1,8 +1,9 @@
 (()=>{
   const params=new URLSearchParams(location.search);
   const requested=Number(params.get('chapter')||1);
-  const selected=[1,2,3].includes(requested)?requested:1;
+  const selected=[1,2,3,4].includes(requested)?requested:1;
   window.SELECTED_CHAPTER=selected;
   if(selected===2)window.CHAPTER_1=window.CHAPTER_2;
   if(selected===3)window.CHAPTER_1=window.CHAPTER_3;
+  if(selected===4)window.CHAPTER_1=window.CHAPTER_4;
 })();
